9954277a4ea2347cb43b063efe6ac0c5c5915231
iulon <i.magularia357@gmail.com>
2023-07-12 23:02:38 +0400

    v2

---
+++ app.js
@@ -0,0 +1,7 @@
+//homeWork
+let peppersNumber = 23; //number
+let goldWeight = 0.05; //fraction
+let petName = "bob"; //string
+let isDoctor = false; //boolean
+let isTeacher = true; //boolean
+let myReservation = undefined; //undefined
