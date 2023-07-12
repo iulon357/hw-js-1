e937924ff548b1aa811c6a16f282b3f60c913990
iulon <i.magularia357@gmail.com>
2023-07-12 23:37:33 +0400

    v3

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ let petName = "bob"; //string
 let isDoctor = false; //boolean
 let isTeacher = true; //boolean
 let myReservation = undefined; //undefined
+
+const myName = "iulon";
+const myHobby = "powerlifting";
+const yearOfBirth = 1988;
+const currentYear = 2023;
+const myAge = currentYear - yearOfBirth;
+
+//v1
+console.log ("My name is " + myName + "," + " I am " +(currentYear-yearOfBirth)+ " years old and my hobby is " + myHobby + ".");
+
+//v2
+console.log ("My name is " + myName + "," + " I am " + myAge + " years old and my hobby is " + myHobby + ".");
+
+//v3
+console.log (`My name is ${myName}, I am ${currentYear - yearOfBirth} years old and my hobby is ${myHobby}.`);
+
+//v4
+console.log (`My name is ${myName}, I am ${myAge} years old and my hobby is ${myHobby}.`);
+
+//example
+console.log (peppersNumber, goldWeight, petName, isDoctor, isTeacher, myReservation);
+console.log (typeof peppersNumber, typeof goldWeight, typeof petName, typeof isDoctor, typeof isTeacher, typeof myReservation);
+
